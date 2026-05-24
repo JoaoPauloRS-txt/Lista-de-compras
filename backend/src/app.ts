843f02abe0b7d6cf509588a54e3c9334d6d62dc1
JoaoPaulo-txt <joaopauloreis-santos@hotmail.com>
2026-05-23 22:55:42 -0300

    adds cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,14 @@
 import type { Express } from "express";
 import express from "express";
 import routes from "./routes.js";
+import cors from "cors";
 
 const app: Express = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 
